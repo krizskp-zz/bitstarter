ac6ae49e5bb2fc9cee552e153c6f948a6552bbe9
krizskp <ubuntu@ip-172-31-4-110.us-west-2.compute.internal>
2013-07-16 10:02:48 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var indexhtml = "index.html";
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync(indexhtml, 'utf-8');
 });
 
 var port = process.env.PORT || 5000;
